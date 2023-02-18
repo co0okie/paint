3346ff0d775ca851e0020f982a506080666debfb
co0okie <cookiequp3@gmail.com>
2023-02-19 01:15:45 +0800

    change keypress logic. decide features. add graphic tablet drag move, zoom features.

--- main.js
+++ main.js
@@ -1,23 +1,32 @@
-let debugInfo = document.getElementById('debug');
+/** features
+ * pen/erase
+ * line width
+ * color
+ * zoom
+ * move/center
+ * undo/redo
+ * save/load
+ * select
+ * cut/copy/paste
+ * expand
+ */
 
 let imageCanvas = document.createElement('canvas');
 let imageContext = imageCanvas.getContext('2d');
-let frameCanvas = document.querySelector('canvas');
-let frameContext = frameCanvas.getContext('2d');
-
-frameCanvas.width = window.innerWidth;
-frameCanvas.height = window.innerHeight;
-
 imageCanvas.width = 1920;
 imageCanvas.height = 1080;
-
-let scale =  imageCanvas.width / frameCanvas.width;
-
 imageContext.lineWidth = 3;
 imageContext.lineCap = 'round';
 imageContext.lineJoin = 'round';
 
-let drawing = false, moving = false;
+let frameCanvas = document.querySelector('canvas');
+let frameContext = frameCanvas.getContext('2d');
+frameCanvas.width = window.innerWidth;
+frameCanvas.height = window.innerHeight;
+
+let scale =  imageCanvas.width / frameCanvas.width;
+
+let mousePressing = [false, false, false]; // left, wheel, right
 
 let frameX = 0, frameY = 0; // frame in image
 let oldPageX, oldPageY;
@@ -25,6 +34,9 @@ let mouseX, mouseY; // mouse in image
 
 print();
 
+/**
+ * @param {Event} e
+ */
 function setMousePosition(e) {
     mouseX = e.pageX * scale + frameX;
     mouseY = e.pageY * scale + frameY;
@@ -40,79 +52,106 @@ function print() {
     );
 }
 
-function startDraw(e) {
-    imageContext.beginPath();
-    imageContext.moveTo(mouseX, mouseY);
-    
-    drawing = true;
-}
-
-function draw(e) {
+function draw() {
     imageContext.lineTo(mouseX, mouseY);
     imageContext.stroke();
-    
     print();
 }
 
+/**
+ * @param {Event} e
+ */
 function move(e) {
     frameX += (oldPageX - e.pageX) * scale;
     frameY += (oldPageY - e.pageY) * scale;
-    
+    print();
+}
+
+/**
+ * @param {Event} e
+ * 
+ * formula: {@link setMousePosition}
+ * ```math
+ * m = mouse, p = page, f = frame, s = scale  
+ * m = p * s + f  
+ * f = m - p * s
+ * ```
+ */
+function wheelZoom(e) {
+    scale *= e.deltaY < 0 ? 0.8 : 1.25; // zoom in : out
+    frameX = mouseX - e.pageX * scale;
+    frameY = mouseY - e.pageY * scale;
+    print();
+}
+
+/**
+ * @param {Event} e 
+ * 
+ * fix at center
+ */
+function mouseZoom(e) {
+    let rate = Math.pow(1.005, oldPageY - e.pageY);
+    frameX += frameCanvas.width / 2 * scale * (1 - rate);
+    frameY += frameCanvas.height / 2 * scale * (1 - rate);
+    scale *= rate;
     print();
 }
 
 document.addEventListener('mousedown', e => {
-    switch (e.button) {
-        case 0: // left click
-            startDraw(e);
-            break;
-        case 1: // wheel click
-            e.preventDefault();
-            moving = true;
-            break;
+    if (e.button == 1) e.preventDefault();
+    
+    if (e.button == 0) {
+        imageContext.beginPath();
+        imageContext.moveTo(mouseX, mouseY);
     }
+    
+    mousePressing[e.button] = true;
 });
 
 document.addEventListener('mousemove', e => {
     setMousePosition(e);
-    if (moving) move(e);
-    else if (drawing) draw(e);
+    
+    if (mousePressing[1] || (e.altKey && mousePressing[0])) {
+        move(e);
+    } else if (e.ctrlKey && mousePressing[0]) {
+        mouseZoom(e);
+    } else if (mousePressing[0]) {
+        draw();
+    }
+    
     oldPageX = e.pageX;
     oldPageY = e.pageY;
 });
 
 document.addEventListener('mouseup', e => {
-    switch (e.button) {
-        case 0: // left click
-            drawing = false;
-            break;
-        case 1: // wheel click
-            moving = false;
-            break;
-    }
+    mousePressing[e.button] = false;
+});
+
+document.addEventListener('keydown', e => {
+    
+});
+
+document.addEventListener('keyup', e => {
+    
 });
 
 frameCanvas.addEventListener('wheel', e => {
     e.preventDefault();
     
-    /**
-     * m = mouse, p = page, f = frame, s = scale, r = rate
-     * m = p * s + f
-     * f = m - p * s
-     * f' = f + m - p * r * s - f
-     *    = f += m - p * r * s - m + p * s
-     *    = f += p * s * (1 - r)
-     */
-    
-    let rate = e.deltaY < 0 ? 0.8 : 1.25; // zoom in : out
-    frameX += e.pageX * scale * (1 - rate);
-    frameY += e.pageY * scale * (1 - rate);
-    scale *= rate;
-    
+    wheelZoom(e);
+});
+
+window.addEventListener('resize', () => {
+    frameCanvas.width = window.innerWidth;
+    frameCanvas.height = window.innerHeight;
     print();
 });
 
+
+
 // debug
+let debugInfo = document.getElementById('debug');
+
 function updateDebugInfo(e) {
     debugInfo.innerHTML = `
         pageX: ${e.pageX}<br>
