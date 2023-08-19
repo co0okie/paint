e4540dda50243f9e53b357b2e4b9c026d84967e2
co0okie <cookiequp3@gmail.com>
2023-08-19 16:46:40 +0800

    unlimited canvas version, complete draw & drag

--- main.js
+++ main.js
@@ -1,249 +1,197 @@
-/** features
- * pen/erase
- * line width
- * color
- * zoom
- * move/center
- * undo/redo
- * save/load
- * select
- * cut/copy/paste
- * expand
- */
-
-const canvas = document.getElementById('main');
-const context = canvas.getContext('2d');
-canvas.width = 1920;
-canvas.height = 10800;
-context.lineWidth = 3;
-context.lineCap = 'round';
-context.lineJoin = 'round';
-context.strokeStyle = 'white';
-
-const body = document.body;
-
-/** {@link setTransform} */
-let scale = window.innerWidth / canvas.width;
-let initScale = scale;
-let translateX = 0, translateY = 0;
-
-let oldClientX, oldClientY;
-let mouseX, mouseY; // mouse in image
-
-const commands = [];
-let command = [], redo = [];
-
-const setting = {
-    stayInPage: false
-};
-
-/**
- * 0: nothing  
- * 1: drawing  
- * 2: moving  
- * 3: zooming  
- */
-let action = 0;
-const ACTION_TEXT = ['nothing', 'drawing', 'moving', 'zooming'];
-
-setTransform();
-
-/** @param {MouseEvent} e */
-function setMouse(e) {
-    mouseX = (e.clientX - translateX) / scale;
-    mouseY = (e.clientY - translateY) / scale;
-}
-
-function setTransform() {
-    if (setting.stayInPage && scale < initScale) scale = initScale;
-    canvas.style.width = canvas.width * scale + 'px';
-    if (setting.stayInPage) {
-        if (translateX > 0) translateX = 0;
-        let rightLimit = window.innerWidth - canvas.offsetWidth;
-        if (translateX < rightLimit) translateX = rightLimit;
+const canvas = new (function () {
+    this.element = document.getElementById('main');
+    this.element.width = 0;
+    this.element.height = 0;
+    
+    this.context = this.element.getContext('2d');
+    this.context.lineWidth = 3;
+    this.context.lineCap = 'round';
+    this.context.lineJoin = 'round';
+    this.context.strokeStyle = 'white';
+    
+    // relative to screen
+    this.originPoint = {x: 0, y: 0};
+    // relative to originPoint
+    this.bound = undefined;
+    
+    this.command = [];
+    this.draw = function ([path, bound]) {
+        // screen coordinte => origin coordinate
+        path = path.map(([x, y]) => [x - this.originPoint.x, y - this.originPoint.y]);
+        bound.left -= this.originPoint.x;
+        bound.right -= this.originPoint.x;
+        bound.top -= this.originPoint.y;
+        bound.bottom -= this.originPoint.y;
+        
+        // first draw
+        if (this.bound === undefined) {
+            this.bound = bound;
+            
+            let lineWidth = this.context.lineWidth;
+            let lineCap = this.context.lineCap;
+            let lineJoin = this.context.lineJoin;
+            let strokeStyle = this.context.strokeStyle;
+            
+            this.element.width = bound.right - bound.left;
+            this.element.height = bound.bottom - bound.top;
+            this.element.style.left = `${this.originPoint.x + bound.left}px`;
+            this.element.style.top = `${this.originPoint.y + bound.top}px`;
+            
+            this.context.lineWidth = lineWidth;
+            this.context.lineCap = lineCap;
+            this.context.lineJoin = lineJoin;
+            this.context.strokeStyle = strokeStyle;
+            
+            this.context.translate(-bound.left, -bound.top);
+        }
+        // out of bounds
+        else if (bound.left < this.bound.left || bound.right > this.bound.right ||
+                 bound.top < this.bound.top || bound.bottom > this.bound.bottom) {
+            bound.left = Math.min(this.bound.left, bound.left);
+            bound.right = Math.max(this.bound.right, bound.right);
+            bound.top = Math.min(this.bound.top, bound.top);
+            bound.bottom = Math.max(this.bound.bottom, bound.bottom);
+            
+            let newCanvas = document.createElement('canvas');
+            newCanvas.width = bound.right - bound.left;
+            newCanvas.height = bound.bottom - bound.top;
+            newCanvas.id = this.element.id;
+            newCanvas.style.left = `${this.originPoint.x + bound.left}px`;
+            newCanvas.style.top = `${this.originPoint.y + bound.top}px`;
+            
+            let newContext = newCanvas.getContext('2d');
+            newContext.lineWidth = this.context.lineWidth;
+            newContext.lineCap = this.context.lineCap;
+            newContext.lineJoin = this.context.lineJoin;
+            newContext.strokeStyle = this.context.strokeStyle;
+            newContext.translate(-bound.left, -bound.top);
+            newContext.drawImage(canvas.element, this.bound.left, this.bound.top);
+            
+            this.bound = bound;
+            canvas.element.replaceWith(newCanvas);
+            this.element = newCanvas;
+            this.context = newContext;
+        }
+        
+        this.context.beginPath();
+        this.context.moveTo(...path[0]);
+        for (point of path) this.context.lineTo(...point);
+        this.context.stroke();
+        
+        this.command.push(path)
     }
-    canvas.style.left = translateX + 'px';
-    canvas.style.top = translateY + 'px';
-}
-
-function refresh() {
-    context.clearRect(0, 0, canvas.width, canvas.height);
-    context.beginPath();
-    for (let command of commands) {
-        context.moveTo(command[0].x, command[0].y);
-        for (let point of command) {
-            context.lineTo(point.x, point.y);
+    this.translate = function (dx, dy) {
+        this.originPoint.x += dx;
+        this.originPoint.y += dy;
+        if (this.bound !== undefined) {
+            this.element.style.left = `${this.originPoint.x + this.bound.left}px`;
+            this.element.style.top = `${this.originPoint.y + this.bound.top}px`;
         }
     }
-    context.stroke();
+})()
+
+{ // add old mouse position in mousemove event
+    let oldEvent = {};
+    document.addEventListener('mousemove', e => {
+        e.oldClientX = oldEvent.clientX;
+        e.oldClientY = oldEvent.clientY;
+        oldEvent = e;
+    }, true)
 }
 
-/**
- * formula: {@link setMouse}
- * ```math
- * m = mouse, c = client, t = translate, s = scale
- * m = (c - t) * s
- * t = c - m / s
- * ```
- * @param {WheelEvent} e
- */
-function wheelZoom(e) {
-    scale *= e.deltaY < 0 ? 1.25 : 0.8;
-    if (setting.stayInPage && scale < initScale) scale = initScale;
-    translateX = e.clientX - mouseX * scale;
-    translateY = e.clientY - mouseY * scale;
-    setTransform();
+// cast 'newaction' event if click, scroll or press any key
+for (type of ['keydown', 'keyup', 'mousedown', 'mouseup', 'wheel']) {
+    document.addEventListener(type, e => document.dispatchEvent(new CustomEvent('newaction')));
 }
 
-/** fix at center  
- * w = window, t = translate, s = scale, c = center
- * ```math
- * (w / 2 - t) / s = c
- * t = w / 2 - s * c
- * t' = w / 2 - r * s * c
- *    = w / 2 - r * s * (w / 2 - t) / s
- *    = t + (1 - r) * (w / 2 - t)
- * ```
- * @param {MouseEvent} e
- */
-function mouseZoom(e) {
-    let rate = 1.005 ** (e.clientY - oldClientY);
-    let newScale = scale * rate;
-    if (setting.stayInPage && newScale < initScale) {
-        rate = initScale / scale;
-        newScale = initScale;
+// action shunt
+document.addEventListener('mousedown', e => {
+    switch (e.button) {
+        case 0:
+            onDrawStart(e);
+            break;
+        case 1:
+            onDragStart(e);
+            break;
     }
-    scale = newScale;
-    translateX += (1 - rate) * (window.innerWidth / 2 - translateX);
-    translateY += (1 - rate) * (window.innerHeight / 2 - translateY);
-    setTransform();
-}
+})
 
-/** @param {MouseEvent} e */
-function onMouseDown(e) {
-    if (e.button === 1) e.preventDefault();
+function onDrawStart(e) {
+    const screenCanvas = document.createElement('canvas');
+    document.body.appendChild(screenCanvas);
+    screenCanvas.style.position = 'fixed'
+    // screenCanvas.style.backgroundColor = '#ffffff33'
+    screenCanvas.style.height = '100%';
+    screenCanvas.style.width = '100%';
+    screenCanvas.width = screenCanvas.offsetWidth;
+    screenCanvas.height = screenCanvas.offsetHeight;
     
-    if (action === 0) {
-        if (e.altKey && e.button === 0 || e.button === 1) {
-            action = 2; // moving
-        } else if (e.ctrlKey && e.button === 0) {
-            action = 3; // zooming
-        } else if (e.button === 0) {
-            action = 1; // drawing
-            command = [];
-            commands.push(command);
-            redo = [];
-            command.push({
-                x: mouseX,
-                y: mouseY
-            });
-        }
-    }
-}
-
-/** @param {MouseEvent} e */
-function onMouseUp(e) {
-    action = 0;
-}
-
-/** @param {MouseEvent} e */
-function onMouseMove(e) {
-    setMouse(e);
+    const screenContext = screenCanvas.getContext('2d');
+    screenContext.lineWidth = canvas.context.lineWidth;
+    screenContext.strokeStyle = canvas.context.strokeStyle;
+    screenContext.lineCap = 'round';
+    screenContext.lineJoin = 'round';
     
-    switch (action) {
-    case 1: // draw
-        command.push({
-            x: mouseX,
-            y: mouseY
-        });
-        refresh();
-        break;
-    case 2: // move
-        translateX += e.clientX - oldClientX;
-        translateY += e.clientY - oldClientY;
-        setTransform();
-        break;
-    case 3: // zoom
-        mouseZoom(e);
-        break;
+    const bound = {left: e.clientX, right: e.clientX, top: e.clientY, bottom: e.clientY}
+    const path = [[e.clientX, e.clientY]];
+    function onDraw(e) {
+        screenContext.beginPath();
+        screenContext.moveTo(e.oldClientX, e.oldClientY);
+        screenContext.lineTo(e.clientX, e.clientY);
+        screenContext.stroke();
+        
+        if (e.clientX < bound.left) bound.left = e.clientX;
+        else if (e.clientX > bound.right) bound.right = e.clientX;
+        if (e.clientY < bound.top) bound.top = e.clientY;
+        else if (e.clientY > bound.bottom) bound.bottom = e.clientY;
+        
+        path.push([e.clientX, e.clientY]);
+    }
+    document.addEventListener('mousemove', onDraw)
+    
+    document.addEventListener('newaction', e => {
+        canvas.draw([path, bound]);
+        screenCanvas.remove();
+        document.removeEventListener('mousemove', onDraw);
+    }, { once: true });
+}
+
+function onDragStart(e) {
+    const onDrag = e => canvas.translate(e.clientX - e.oldClientX, e.clientY - e.oldClientY);
+    document.addEventListener('mousemove', onDrag);
+    document.addEventListener('newaction', e => document.removeEventListener('mousemove', onDrag));
+}
+
+{ // debug
+    const debugInfo = document.getElementById('debug');
+    
+    const originPoint = document.createElement('div');
+    originPoint.style.backgroundColor = 'red';
+    originPoint.style.position = 'fixed';
+    originPoint.style.width = '20px';
+    originPoint.style.height = '20px';
+    originPoint.style.borderRadius = '50%';
+    document.body.appendChild(originPoint);
+    
+    function updateDebugInfo(e) {
+        originPoint.style.left = `${canvas.originPoint.x}px`;
+        originPoint.style.top = `${canvas.originPoint.y}px`;
+        debugInfo.innerHTML = `
+            pageX: ${e.pageX}<br>
+            pageY: ${e.pageY}<br>
+            clientX: ${e.clientX}<br>
+            clientY: ${e.clientY}<br>
+            transform: ${canvas.element.style.transform}<br>
+            canvas.offsetWidth: ${canvas.element.offsetWidth}<br>
+            canvas.offsetHeight: ${canvas.element.offsetHeight}<br>
+            window.innerWidth: ${window.innerWidth}<br>
+            window.innerHeight: ${window.innerHeight}<br>
+            command.stack.length: ${canvas.command.length}<br>
+        `;
     }
     
-    oldClientX = e.clientX;
-    oldClientY = e.clientY;
+    document.addEventListener('mousemove', updateDebugInfo);
+    document.addEventListener('mousedown', updateDebugInfo);
+    document.addEventListener('mouseup', updateDebugInfo);
+    document.addEventListener('wheel', updateDebugInfo);
 }
-
-/** @param {KeyboardEvent} e */
-function onKeyDown(e) {
-    if (e.ctrlKey && e.code === 'KeyZ') {
-        if (e.shiftKey) {
-            if (redo.length) commands.push(redo.pop());
-        } else {
-            if (commands.length) redo.push(commands.pop());
-        }
-        refresh();
-    }
-}
-
-/** @param {KeyboardEvent} e */
-function onKeyUp(e) {
-    if (e.code === 'KeyL') {
-        setting.stayInPage = !setting.stayInPage;
-        setTransform();
-    }
-    if (e.ctrlKey && e.code === 'KeyC') {
-        canvas.toBlob(blob => navigator.clipboard.write([new ClipboardItem({[blob.type]: blob})]));
-    }
-}
-
-/** @param {WheelEvent} e */
-function onWheel(e) {
-    e.preventDefault();
-    wheelZoom(e);
-}
-
-function onResize() {
-    initScale = window.innerWidth / canvas.width;
-    setTransform();
-}
-
-document.addEventListener('mousemove', e => {
-    setMouse(e);
-    oldClientX = e.clientX;
-    oldClientY = e.clientY;
-    
-    document.addEventListener('mousedown', onMouseDown);
-    document.addEventListener('mouseup', onMouseUp);
-    document.addEventListener('mousemove', onMouseMove);
-    document.addEventListener('keydown', onKeyDown);
-    document.addEventListener('keyup', onKeyUp);
-    document.addEventListener('wheel', onWheel, { passive: false });
-    window.addEventListener('resize', onResize);
-}, { once: true });
-
-// debug
-let debugInfo = document.getElementById('debug');
-
-function updateDebugInfo(e) {
-    debugInfo.innerHTML = `
-        pageX: ${e.pageX}<br>
-        pageY: ${e.pageY}<br>
-        clientX: ${e.clientX}<br>
-        clientY: ${e.clientY}<br>
-        mouseX: ${mouseX}<br>
-        mouseY: ${mouseY}<br>
-        scale: ${scale}<br>
-        translateX: ${translateX}<br>
-        translateY: ${translateY}<br>
-        points.length ${command.length}<br>
-        record.length: ${commands.length}<br>
-        redo.length: ${redo.length}<br>
-        action: ${ACTION_TEXT[action]}<br>
-        transform: ${canvas.style.transform}<br>
-        canvas.offsetWidth: ${canvas.offsetWidth}<br>
-        translateX + canvas.offsetWidth: ${translateX + canvas.offsetWidth}<br>
-        window.innerWidth: ${window.innerWidth}<br>
-    `;
-}
-
-// document.addEventListener('mousemove', updateDebugInfo);
-// document.addEventListener('wheel', updateDebugInfo);
